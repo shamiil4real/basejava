73986f1564c850c151c2228ccfea48cafc72f0dd
shamiil <wonderboy2k.yt@gmail.com>
2023-03-13 14:45:56 +0700

    Изменил структуру папок

---
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -0,0 +1,93 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size;
+
+    void update(Resume r) {
+        if (isPresent(r.getUuid())) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(r.getUuid())) {
+                    storage[i] = r;
+                }
+            }
+        } else {
+            System.out.println("Ошибка! Разюме " + r.getUuid() + " не существует");
+        }
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (size >= storage.length) {
+            System.out.println("Ошибка! Нет места для сохранения разюме");
+        } else {
+            if (!isPresent(r.getUuid())) {
+                storage[size] = r;
+                size++;
+            } else {
+                System.out.println("Ошибка! Разюме " + r.getUuid() + " уже существует");
+            }
+        }
+    }
+
+    public Resume get(String uuid) {
+        if (isPresent(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        } else {
+            System.out.println("Ошибка! Разюме " + uuid + " не существует");
+        }
+        return null;
+    }
+
+    public void delete(String uuid) {
+        if (isPresent(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(uuid)) {
+                    for (int j = i; j < size - 1; j++) {
+                        storage[j] = storage[j + 1];
+                    }
+                    storage[size] = null;
+                    size--;
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Ошибка! Разюме " + uuid + " не существует");
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    boolean isPresent(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/com/basejava/webapp/storage/MainArray.java
+++ src/com/basejava/webapp/storage/MainArray.java
@@ -1,9 +1,13 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Interactive test for ArrayStorage implementation
+ * Interactive test for com.basejava.webapp.storage.ArrayStorage implementation
  * (just run, no need to understand)
  */
 public class MainArray {
@@ -32,7 +36,7 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.uuid = uuid;
+                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
