729dacc5379ba6d07a253e8025ebb900bbf46852
shamiil <wonderboy2k.yt@gmail.com>
2023-03-19 17:09:54 +0700

    String

--- src/com/basejava/webapp/MainString.java
+++ src/com/basejava/webapp/MainString.java
@@ -1,4 +1,12 @@
 package com.basejava.webapp;
 
 public class MainString {
+    public static void main(String[] args) {
+        String[] strArray = new String[]{"1", "2", "3", "4", "5"};
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArray) {
+            sb.append(str).append(", ");
+        }
+        System.out.println(sb);
+    }
 }
